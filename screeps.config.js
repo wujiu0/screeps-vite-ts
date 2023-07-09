9bfd3ed84edb79a2a524a39584f23aff6bf1b1ae
suidz <1050705928@qq.com>
2023-07-09 15:36:47 +0800

    init

---
+++ screeps.config.js
@@ -0,0 +1,8 @@
+export const screepsConfig = {
+    // build outputDir
+    sourceDir: 'dist',
+
+    // copy targetDir(game scripts folder)
+    targetDir: 'C:\\Users\\Wujiu\\AppData\\Local\\Screeps\\scripts\\screeps.com\\default'
+}
+
