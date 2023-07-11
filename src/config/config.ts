434a60fe85f4a94cd216a4a2ab9e90dec29b3041
suidz <1050705928@qq.com>
2023-07-11 21:55:26 +0800

    提取公共配置

---
+++ src/config/config.ts
@@ -0,0 +1,20 @@
+const config = {
+  SPAWN_INIT_CONFIG: {
+    harvester: 0,
+    upgrader: 0,
+    builder: 0,
+    communicator: 0,
+    pioneer: 0,
+    temp: 0,
+  },
+  SPAWN_MAX_CONFIG: {
+    harvester: 3,
+    upgrader: 1,
+    builder: 10,
+    communicator: 1,
+    pioneer: 1,
+    temp: 99,
+  },
+};
+
+export default config;
